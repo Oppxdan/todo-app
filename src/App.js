f46e44a046532f215806c3adfd537ac8934b70f0
Aaron Regalado <regalado2829@gmail.com>
2023-03-19 22:00:03 -0400

    Starting to Set Up Mapping
    Setting up part of project that maps through the to do list and returns
    JSX componenets.

--- src/App.js
+++ src/App.js
@@ -6,14 +6,30 @@ function App() {
   const [toDoList, setToDoList] = useState([]);
   const [task, setTask] = useState("");
 
+  const changeTask = (event) => {
+    setTask(event.target.value);
+  }
+
+  const addTask = () => {
+    const newList = [...toDoList, {id: toDoList.length == 0 ? 1 : toDoList[toDoList.length - 1].id + 1, taskName: task, isComplete: false}]
+
+    setToDoList()
+  }
+
   return (
     <div className="App">
       <div className="task-input">
-        <input />
-        <button>Add Task</button>
+        <input onChange={changeTask} />
+        <button onClick={addTask}>Add Task</button>
       </div>
       <div className="task-list">
-        
+        {toDoList.map((task) => {
+          return (
+            <div>
+              <p></p>
+            </div>
+          )
+        })}
       </div>
     </div>
   );
