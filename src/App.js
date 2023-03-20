bf64b80801730212ff19cf9021a303d7a55c64ef
Aaron Regalado <regalado2829@gmail.com>
2023-03-19 21:07:29 -0400

    Starting on the To Do List
    Starting the setup for the to do list [states and initial JSX].

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [toDoList, setToDoList] = useState([]);
+  const [task, setTask] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="task-input">
+        <input />
+        <button>Add Task</button>
+      </div>
+      <div className="task-list">
+        
+      </div>
     </div>
   );
 }
