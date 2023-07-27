a09776017a611cdbc3ea13271b44febc5edbd1da
Aaron Regalado <regalado2829@gmail.com>
2023-07-26 20:16:13 -0400

    Adding Edit Task + Accessibility

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import {useState} from 'react';
+import { useState } from 'react';
 import './App.css';
 import NormalTask from './components/normalTask/normal-task';
 
@@ -13,62 +12,82 @@ function App() {
 
   const addTask = () => {
     if (task !== "") {
-      const newList = [...toDoList, {id: toDoList.length == 0 ? 1 : toDoList[toDoList.length - 1].id + 1, taskName: task, isComplete: false, isEditing: false}]
-
+      const newList = [...toDoList, { id: toDoList.length === 0 ? 1 : toDoList[toDoList.length - 1].id + 1, taskName: task, isComplete: false, isEditing: false }]
       setToDoList(newList);
     }
   }
 
   const deleteTask = (id) => {
     const deletedList = toDoList.filter((task) => {
-      if (task.id == id) {
+      if (task.id === id) {
         return false;
       } else {
         return true;
       }
     })
-
     setToDoList(deletedList);
   }
 
   const completeTask = (id) => {
     const completedTaskList = toDoList.map((task) => {
-      if (task.id == id) {
-        return {...task, isComplete: true}
+      if (task.id === id) {
+        return { ...task, isComplete: true }
       } else {
         return task
       }
     });
-
     setToDoList(completedTaskList);
   }
+
+  const editTask = (taskId, updatedTaskName) => {
+    const updatedTasks = toDoList.map((task) =>
+      task.id === taskId ? { ...task, taskName: updatedTaskName } : task
+    );
+    setToDoList(updatedTasks); // Update the toDoList state, not task state
+  };
+
 
   return (
     <div className="App">
       <div className="TodoWrapper">
         <h1 className="heading">Let's Get Things Done!</h1>
-        <div className="task-input">
-          <input className="todo-input" onChange={changeTask} placeholder="What's the Task for Today?"/>
-          <button className="todo-btn" onClick={addTask}>Add Task</button>
-        </div>
-        <div className="task-list">
-          {toDoList.map((task) => {
-            return (
-              <NormalTask 
-                isComplete={task.isComplete}
-                id={task.id}
-                taskName={task.taskName}
-                addTask={addTask}
-                deleteTask={deleteTask}
-                toDoList={toDoList}
-                setToDoList={setToDoList}
-                task={task}
-                setTask={setTask}
-                completeTask={completeTask}
-              />
-            )
-          })}
-        </div>
+        <section className="task-input">
+          <input
+            id="todo-input"
+            className="todo-input"
+            onChange={changeTask}
+            value={task}
+            placeholder="Enter your task here"
+            aria-label="Task Input"
+            aria-required="true"
+          />
+          <button
+            className="todo-btn"
+            onClick={addTask}
+            aria-label="Add Task Button"
+          >
+            Add Task
+          </button>
+        </section>
+        <section className="task-list">
+          {toDoList.map((task) => (
+            <NormalTask
+              key={task.id}
+              isComplete={task.isComplete}
+              id={task.id}
+              taskName={task.taskName}
+              addTask={addTask}
+              deleteTask={deleteTask}
+              toDoList={toDoList}
+              setToDoList={setToDoList}
+              editTask={editTask}
+              task={task}
+              setTask={setTask}
+              completeTask={completeTask}
+              aria-label={`Task: ${task.taskName}, Status: ${task.isComplete ? 'Completed' : 'Not Completed'}`}
+            />
+          ))}
+        </section>
       </div>
     </div>
   );
--- src/components/normalTask/normal-task.jsx
+++ src/components/normalTask/normal-task.jsx
@@ -1,13 +1,74 @@
-import {useState} from 'react';
-import './task.css'
+import React, { useState } from 'react';
+import './task.css';
 
 function NormalTask(props) {
-  
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTaskName, setEditedTaskName] = useState(props.taskName);
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleSave = () => {
+    props.editTask(props.id, editedTaskName);
+    setIsEditing(false);
+  };
+
+  const handleCancel = () => {
+    setIsEditing(false);
+    setEditedTaskName(props.taskName);
+  };
+
+  const handleInputChange = (event) => {
+    setEditedTaskName(event.target.value);
+  };
+
+  const labelEditInput = 'Edit task input';
+
   return (
-    <div className="task-container" style={{backgroundColor : props.isComplete ? "green" : "#8758ff"}}>
-        <p className="task-name" key={props.id}>{props.taskName}</p>
-        <button className="task-btn" onClick={() => props.completeTask(props.id)}>✅</button>
-        <button className="task-btn" onClick={() => props.deleteTask(props.id)}>🗑️</button>
+    <div
+      className="task-container"
+      style={{ backgroundColor: props.isComplete ? "green" : "#8758ff" }}
+      role="listitem"
+    >
+      {isEditing ? (
+        <div>
+          <label htmlFor={`editInput_${props.id}`} className="sr-only">
+            {labelEditInput}
+          </label>
+          <input
+            type="text"
+            id={`editInput_${props.id}`}
+            value={editedTaskName}
+            onChange={handleInputChange}
+            className="edit-task-input"
+            aria-label={labelEditInput}
+          />
+          <button className="task-btn" onClick={handleSave}>Save</button>
+          <button className="task-btn" onClick={handleCancel}>Cancel</button>
+        </div>
+      ) : (
+        <div>
+          <p className="task-name" key={props.id}>{editedTaskName}</p>
+          <button
+            className="task-btn"
+            onClick={() => props.completeTask(props.id)}
+            aria-label="Mark as complete"
+          >
+            ✅
+          </button>
+          <button
+            className="task-btn"
+            onClick={() => props.deleteTask(props.id)}
+            aria-label="Delete task"
+          >
+            🗑️
+          </button>
+          <button className="task-btn" onClick={handleEdit} aria-label="Edit task">
+            Edit
+          </button>
+        </div>
+      )}
     </div>
   );
 }
